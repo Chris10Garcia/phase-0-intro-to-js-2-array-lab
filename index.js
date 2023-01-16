7278b04c1a32f7675a51e2d9cf51fddf6be59f86
Christian Garcia <bones192nd@gmail.com>
2023-01-16 14:28:12 -0500

    redo of phase0 lab but using arrows

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => cats.push(name)
+
+const destructivelyPrependCat = name => cats.unshift(name)
+
+const destructivelyRemoveLastCat = () => cats.pop()
+
+const destructivelyRemoveFirstCat = () => cats.shift()
+
+const appendCat = name => [...cats, name]
+
+const prependCat = name => [name, ...cats]
+
+const removeLastCat = () => cats.slice(0,cats.length-1)
+//remember, with slice(x,y), y is non inclusive
+
+const removeFirstCat = () => cats.slice(1, cats.length)
+//remember, with slice(x,y), y is non inclusive
+
+// function destructivelyAppendCat(name){
+//     return cats.push(name)
+// }
